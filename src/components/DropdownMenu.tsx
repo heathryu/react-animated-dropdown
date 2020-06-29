73e2a0d74c23457e6b0dd8cdd8da1088f1faa8cd
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2020-06-29 20:58:45 +0100

    Add more menu items and add resizing animation for dropdown

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 import { ReactComponent as ArrowIcon } from '../icons/arrow.svg';
+import { ReactComponent as BoltIcon } from '../icons/bolt.svg';
 import { ReactComponent as CogIcon } from '../icons/cog.svg';
 import { ReactComponent as ChevronIcon } from '../icons/chevron.svg';
 
@@ -9,23 +10,39 @@ import DropdownItem from './DropdownItem';
 
 const DropdownMenu = () => {
   const [activeMenu, setActiveMenu] = useState<string>('main');
+  const [menuHeight, setMenuHeight] = useState<number>();
+
+  const calcHeight = (el: HTMLElement) => {
+    setMenuHeight(el.offsetHeight);
+  };
 
   return (
-    <div className="dropdown">
+    <div className="dropdown" style={{ height: menuHeight }}>
       <CSSTransition
         in={activeMenu === 'main'}
         unmountOnExit
         timeout={500}
         classNames="menu-primary"
+        onEnter={calcHeight}
       >
         <div className="menu">
           <DropdownItem>My Profile</DropdownItem>
           <DropdownItem
-            goToMenu="settings"
-            setActiveMenu={setActiveMenu}
             leftIcon={<CogIcon />}
             rightIcon={<ChevronIcon />}
-          ></DropdownItem>
+            goToMenu="settings"
+            setActiveMenu={setActiveMenu}
+          >
+            Settings
+          </DropdownItem>
+          <DropdownItem
+            leftIcon="🦧"
+            rightIcon={<ChevronIcon />}
+            goToMenu="animals"
+            setActiveMenu={setActiveMenu}
+          >
+            Animals
+          </DropdownItem>
         </div>
       </CSSTransition>
 
@@ -34,10 +51,42 @@ const DropdownMenu = () => {
         unmountOnExit
         timeout={500}
         classNames="menu-secondary"
+        onEnter={calcHeight}
       >
         <div className="menu">
-          <DropdownItem leftIcon={<ArrowIcon />} goToMenu="main" setActiveMenu={setActiveMenu} />
-          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem
+            leftIcon={<ArrowIcon />}
+            goToMenu="main"
+            setActiveMenu={setActiveMenu}
+          >
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon={<ArrowIcon />}
+            goToMenu="main"
+            setActiveMenu={setActiveMenu}
+          >
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
         </div>
       </CSSTransition>
     </div>
